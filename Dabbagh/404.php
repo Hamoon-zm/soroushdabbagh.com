6e9c5a459139da9d43c9af9442eba93f0cf8b242
Hamoon-zm <70867263+Hamoon-zm@users.noreply.github.com>
2022-07-03 18:12:26 -0400

    Update 404.php

--- Dabbagh/404.php
+++ Dabbagh/404.php
@@ -1,1 +1,17 @@
-hey
+<?php get_header();?>
+<section class="page-wrap">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-center vh-100">
+                <div class="text-center">
+                    <h1 class="display-1 fw-bold">404</h1>
+                    <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                    <p class="lead">
+                        The page you’re looking for doesn’t exist.
+                    </p>
+                    <a href="<?php bloginfo('home');?>/" class="btn btn-secondary">صفحه نخست</a>
+                </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer();?>
